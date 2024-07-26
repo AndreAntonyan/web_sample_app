71c26ced328db27f5567441d6b6082b58782094d
Andre <andreantonyan87@gmail.com>
2024-07-26 19:08:18 +0400

    added comment

--- itproger/main/urls.py
+++ itproger/main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# import path and views
+
 urlpatterns = [
     path('', views.index, name='home'),
     path('about', views.about, name='about')
